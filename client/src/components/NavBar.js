c0c4131a1de1d97dfdab94f9d69c012b92a83010
jayshah <jaywebdev98@gmai.com>
2023-02-01 21:46:14 +0530

    add link tag for landing page button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div className="flex justify-between items-center mx-[4%] mt-[1%] font-Poppins">
@@ -7,8 +9,16 @@ const NavBar = () => {
         className="h-[50px]"
       />
       <div className="flex gap-x-[10px]">
-        <button className="border-[1px] border-blue rounded-[10px] px-[13px] py-[7px] font-medium hover:bg-blue hover:text-white">Find Job</button>
-        <button className="border-[1px] border-blue rounded-[10px] px-[13px] py-[7px] font-medium hover:bg-blue hover:text-white">Post Job</button>
+        <Link to="/candidate">
+          <button className="border-[1px] border-blue rounded-[10px] px-[13px] py-[7px] font-medium hover:bg-blue hover:text-white">
+            Find Job
+          </button>
+        </Link>
+        <Link to="/recruiter">
+          <button className="border-[1px] border-blue rounded-[10px] px-[13px] py-[7px] font-medium hover:bg-blue hover:text-white">
+            Post Job
+          </button>
+        </Link>
       </div>
     </div>
   );
